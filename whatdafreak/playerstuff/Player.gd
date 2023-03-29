a425a13c6c3d69ecf3c4c0a6845b1fb7eeebb023
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-03-28 21:13:28 -0400

    stuff

--- whatdafreak/playerstuff/Player.gd
+++ whatdafreak/playerstuff/Player.gd
@@ -54,8 +54,8 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("moveleft", "moveright", "moveup", "movedown")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * speed * delta
+		velocity.z = direction.z * speed * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
